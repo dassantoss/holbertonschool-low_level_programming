441dc5fd8a2dcfe7c9b4c716ea436da451a5a543
Your Name <you@example.com>
2023-02-17 15:57:25 -0800

    Fix program 101-quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char text[]="and that piece of art is useful\" - Dora korpar, 2015-10-19\n";
-	write(2, text, 59);
+	char quote1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, quote1, 59);
 	return (1);
 }
