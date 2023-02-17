ed19953b7bbc5981d53290bde96c404c7081196b
Your Name <you@example.com>
2023-02-17 14:50:48 -0800

    Fix C program for print with CUT

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include<stdio.h>
-/*main for Print using puts 
-  returns 0 if the process is successful
- * */
+#include<stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print a string to the standard output
+ *
+ * Return Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
