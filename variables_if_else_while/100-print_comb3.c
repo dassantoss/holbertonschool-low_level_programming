d37018811a3ac1f8dd1444b0cb9d18576ca1944f
Your Name <you@example.com>
2023-02-21 20:52:07 -0800

    Fix program that prints all possible different combinations of two digits.

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,20 +9,17 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 10; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digit2 = 1; digit2 < 9; digit2++)
+		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
 		{
-			if (digit1 != digit2)
-			{
-				putchar(digit1 + '0');
-				putchar(digit2 + '0');
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
 
-				if (digit1 != 9 || digit2 != 8)
-				{	
-					putchar(',');
-					putchar(' ');
-				}
+			if (digit1 != 8 || digit2 != 9)
+			{	
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
