795e991b3f891c15624a225c94f032880c39138f
Your Name <you@example.com>
2023-02-21 16:30:22 -0800

    Fix program for print negative and positive numbers

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n == 0)
-		printf("is zero");
+		printf("%d is zero");
 	else if (n > 0)
-		printf("is positive");
+		printf("%d is positive");
 	else
-		printf("is negative");
+		printf("%d is negative");
 
 	return (0);
 }
