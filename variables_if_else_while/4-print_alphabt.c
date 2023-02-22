44e127e5b535e5b07e626ea40dccb157ec484b73
Your Name <you@example.com>
2023-02-21 18:59:00 -0800

    Add program for print alphabet except q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
