4f32d34569906734ebb41634b2ff1865c2ae7e0a
Your Name <you@example.com>
2023-02-22 19:46:21 -0800

    Add function that prints the alphabet, in lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
