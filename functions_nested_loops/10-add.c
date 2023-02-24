0b6782c890bb3f07dd1281ed47459fc1b4d33614
Your Name <you@example.com>
2023-02-23 18:21:09 -0800

    Add function that adds two integers and returns the result.

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result.
+ *
+ * @n1: Number 1
+ * @n2: Number 2
+ *
+ * Return: Return integer
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
