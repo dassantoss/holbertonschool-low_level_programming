f332498af43cd5c838f465fd1084f2c5bbba3616
Your Name <you@example.com>
2023-02-24 18:59:20 -0800

    Fix File 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_diagonal -  function that draws a diagonal line on the terminal.
+ * print_diagonal - function that draws a diagonal line on the terminal.
  *
  *@n: Input Number
  *
