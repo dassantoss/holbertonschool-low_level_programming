15190c03d4255a6b9041fb9fc0e205b2c1286d13
Your Name <you@example.com>
2023-02-28 18:46:36 -0800

    Fix fuction thats print string in reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -14,9 +14,9 @@ void print_rev(char *s)
 	while (*(s + cont) != '\0')
 		cont++;
 
-	while (cont > 0)
+	while (cont >= 0)
 	{
-		_putchar(*(s + cont));
+		_puthar(*(s + cont));
 		cont--;
 	}
 	_putchar('\n');
