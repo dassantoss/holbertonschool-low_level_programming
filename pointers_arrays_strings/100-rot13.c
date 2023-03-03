b3c08191a880cf2e66acc80103661652b444e856
Your Name <you@example.com>
2023-03-03 14:51:25 -0800

    Add function that encode a string using rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+/**
+ * rot13 - Function that encode a string using rot13
+ *
+ * @str: Input string to be encode
+ *
+ * Return: Return encode string
+ */
+char *rot13(char *str)
+{
+	char *ptr = str;
+	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	while (*ptr)
+	{
+		j = 0;
+		while (*(alpha + j))
+		{
+			if (*ptr == *(alpha + j))
+			{
+				*ptr = *(rot13 + j);
+				break;
+			}
+			j++;
+		}
+		ptr++;
+	}
+	return (str);
+}
