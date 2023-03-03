fe786367f65b0211c30cbdcec9ff569d41484dca
Your Name <you@example.com>
2023-03-03 11:34:42 -0800

    Fix Function that capitalizes all words of an string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@
 char *cap_string(char *str)
 {
 	char *ptr = str;
-	int i, j, sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i, sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 	int sep_word_size = 13;
 
 	while (*ptr)
