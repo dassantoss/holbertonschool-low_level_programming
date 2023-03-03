835ccfb07b313f554dffa3158da45998f0431536
Your Name <you@example.com>
2023-03-02 17:23:50 -0800

    Add function that concatenates two strings.

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings.
+ *
+ * @dest: destination
+ * @src: source
+ *
+ * Return: Poiter to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (*src != '\0')
+	{
+		*ptr++ = *src++;
+	}
+	*ptr = '\0';
+	return (dest);
+}
