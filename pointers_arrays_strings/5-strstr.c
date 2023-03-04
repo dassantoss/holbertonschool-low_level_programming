ff0639eb037617346fc3b09529f5c5de2c8a07b9
Your Name <you@example.com>
2023-03-03 19:16:31 -0800

    Fix function that locates a substring.

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -17,8 +17,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		bhaystack = haystack;
 		pneedle = needle;
-
-	
+		
 		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
 		{
 			haystack++;
