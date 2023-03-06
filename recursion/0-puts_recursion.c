e18371936d05e3517377823dc591b6a9f9b5310d
Your Name <you@example.com>
2023-03-06 15:26:34 -0800

    Fix function that prints a string using recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,7 +1,6 @@
 #include "main.h"
-#include <stdio.h>
 
-/*
+/**
  * _puts_recursion - Function that prints a string using recursion
  *
  * @s: Input string
@@ -11,8 +10,8 @@
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
-	{	
-		_putchar(*s);	
+	{
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
