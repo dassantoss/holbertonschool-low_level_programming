a60b6cc43f2e5dbc5bd71423177318a0f7ed3c39
Your Name <you@example.com>
2023-03-07 12:09:42 -0800

    Update main.h

--- recursion/main.h
+++ recursion/main.h
@@ -8,9 +8,14 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+
 int check_prime(int n, int i);
 int is_prime_number(int n);
+
+int check_palindrome(char *s, int start, int end);
+int _strlen_recursion(char *s);
 int is_palindrome(char *s);
+
 int wildcmp(char *s1, char *s2);
 
 #endif
