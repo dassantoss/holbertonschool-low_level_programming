56ff96ea191f04e9325018650728564f73278f70
Your Name <you@example.com>
2023-03-24 21:15:17 -0700

    Fix program that executes a function given as a parameter on each element of an array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -3,7 +3,7 @@
 /**
  * array_iterator - executes a function given as a
  * parameter on each element of an array.
- * 
+ *
  * @array: input integer array.
  * @size: size of the array.
  * @action: pointer to the function.
