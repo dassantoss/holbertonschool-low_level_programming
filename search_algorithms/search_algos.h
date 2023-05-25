4acf13cdb4bc3a33044a6d719c7e73ed57e4a864
Santiago E <santy.e@gmail.com>
2023-05-25 17:38:17 -0500

    Betty fix

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -4,8 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*function that searches for a value in an array of integers 
-using the Linear search algorithm*/
+/*searches for a value in an array of integers using the Linear search*/
 int linear_search(int *array, size_t size, int value);
 
 
