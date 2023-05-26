d328c11b4744fee4535b2a46096b6c00f836f217
Santiago E <santy.e@gmail.com>
2023-05-26 13:11:14 -0500

    Fix

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -1,43 +1,46 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * jump_search - searches for a value in an array of
- * integers using the Jump search algorithm
+ * jump_search - searches for a value in an array of integers
+ *               using the Jump search algorithm
  *
  * @array: input array
  * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * @value: value to search for
+ *
+ * Return: index of the value, or -1 if not found
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int index, m, k, prev;
+	int jump, prev_index, index;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	m = (int)sqrt((double)size);
-	k = 0;
-	prev = index = 0;
+	jump = (int) sqrt((double) size);
+	prev_index = 0;
+	index = jump;
 
-	do {
-		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+	printf("Value checked array[%d] = [%d]\n", prev_index, array[prev_index]);
 
-		if (array[index] == value)
-			return (index);
-		k++;
-		prev = index;
-		index = k * m;
-	} while (index < (int)size && array[index] < value);
-
-	printf("Value found between indexes [%d] and [%d]\n", prev, index);
-
-	for (; prev <= index && prev < (int)size; prev++)
+	while (index < (int) size && array[index] < value)
 	{
-		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-		if (array[prev] == value)
-			return (prev);
+		prev_index = index;
+		index += jump;
+		printf("Value checked array[%d] = [%d]\n", prev_index, array[prev_index]);
+	}
+
+	printf("Value found between indexes [%d] and [%d]\n", prev_index, index);
+
+	if (index >= (int) size || array[prev_index] > value)
+		return (-1);
+
+	while (prev_index <= index)
+	{
+		printf("Value checked array[%d] = [%d]\n", prev_index, array[prev_index]);
+		if (array[prev_index] == value)
+			return (prev_index);
+		prev_index++;
 	}
 
 	return (-1);
