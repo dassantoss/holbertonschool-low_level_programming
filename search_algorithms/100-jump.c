cc4df27393035bd458f90d916e4bc1c10164e4c8
Santiago E <santy.e@gmail.com>
2023-05-26 12:03:43 -0500

    Fix

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -18,10 +18,10 @@ int jump_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	if (array[curr] == value)
+	if (array[0] == value)
 	{
-		printf("Value checked array[%lu] = [%d]\n", curr, array[curr]);
-		return (curr);
+		printf("Value checked array[0] = [%d]\n", array[0]);
+		return (0);
 	}
 
 	while (curr < size && array[curr] < value)
