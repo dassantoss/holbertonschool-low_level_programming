ee3f29f61f697e9974cb993f979bc66ca1ac472d
Santiago E <santy.e@gmail.com>
2023-05-26 16:44:59 -0500

    Add Linear search in a skip list

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -21,6 +21,24 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
 /*searches for a value in an array of integers using the Linear search*/
 int linear_search(int *array, size_t size, int value);
 
@@ -50,5 +68,8 @@ int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 
 /* Jump search in a singly linked list*/
 listint_t *jump_list(listint_t *list, size_t size, int value);
+
+/*Linear search in a skip list*/
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
